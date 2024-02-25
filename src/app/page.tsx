fa062ff1b265ab332b4753222f811732d3ef30d9
Evelinn <evelinn.idenfors@gmail.com>
2024-02-25 11:46:00 +0000

    passing input to output formatted correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,27 @@
 "use client";
+import { useState } from "react";
+import Tranformation from "./transform";
 
 export default function Home() {
+  let [inputValue, setInputValue] = useState<string>("");
+  let [outputValue, setOutputValue] = useState<string>();
+
   return (
     <main className="">
       <form action="" className="input-form">
         <label htmlFor="">Input</label>
-        <textarea name="message"></textarea>
+        <textarea
+          name="message"
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+        ></textarea>
       </form>
-      <button onClick={() => console.log("clicked")}>Transform</button>
+      <button onClick={() => setOutputValue(Tranformation(inputValue))}>
+        Transform
+      </button>
       <form action="">
         <label htmlFor="">Output</label>
-        <textarea name="message"></textarea>
+        <textarea name="message" value={outputValue}></textarea>
       </form>
     </main>
   );
