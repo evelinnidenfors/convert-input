0e49593daf24f6c1b16d7f08d5b051735dc8fbeb
Evelinn <evelinn.idenfors@gmail.com>
2024-02-26 20:01:53 +0000

    functionality improvements and allowing to copy to clipboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,31 +11,55 @@ export default function Home() {
     setInputValue("");
   };
 
+  function copyText(copy: any) {
+    navigator.clipboard.writeText(copy);
+    event?.preventDefault();
+  }
+
   return (
     <main className="">
-      <form action="" className="input-form">
-        <label htmlFor="">Input</label>
-        <textarea
-          name="message"
-          value={inputValue}
-          onChange={(event) => setInputValue(event.target.value)}
-        ></textarea>
-      </form>
-      <div className="buttons">
-        <button
-          className="button transform"
-          onClick={() => setOutputValue(tranformation(inputValue))}
-        >
-          Transform
-        </button>
-        <button className="button clear" onClick={handleClear}>
-          Clear
-        </button>
+      <div>
+        <form action="" className="input-form">
+          <label htmlFor="">Input</label>
+          <textarea
+            name="message"
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+          ></textarea>
+        </form>
+        <div className="buttons">
+          <button
+            className="button transform"
+            onClick={() => setOutputValue(tranformation(inputValue))}
+          >
+            Transform
+          </button>
+          <button className="button clear" onClick={handleClear}>
+            Clear
+          </button>
+        </div>
+        <form action="" className="output-form">
+          <div>
+            <label htmlFor="">Output</label>
+            <button
+              className="button copy"
+              onClick={() => copyText(outputValue)}
+            >
+              <img src="/copy.png" alt="" />
+            </button>
+          </div>
+          <textarea
+            className="output-text"
+            name="message"
+            value={outputValue}
+          ></textarea>
+        </form>
       </div>
-      <form action="">
-        <label htmlFor="">Output</label>
-        <textarea name="message" value={outputValue}></textarea>
-      </form>
+      {/* <div>
+        <button className="button copy" onClick={() => copyText(outputValue)}>
+          Copy <img src="/copy.png" alt="" />
+        </button>
+      </div> */}
     </main>
   );
 }
--- src/app/transform.ts
+++ src/app/transform.ts
@@ -2,7 +2,7 @@ export default function tranformation(inputText:string) {
     let inputArray = inputText.split("\n");
 
     return inputArray.map((el) => {
-        let removeComma = el.replaceAll(',', '');
+        let removeComma = el.replaceAll(',', '').trim();
         let hasWhitespace = removeComma.indexOf(' ') > 0;
 
         if (hasWhitespace) {
